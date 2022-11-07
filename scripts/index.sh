9ee943171f9d190f2d4d05854539a4f33f0056fc
Tomás Di Domenico <tdido@tdido.eu>
2022-11-07 16:55:03 +0100

    Limit scripts to 80 characters width

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,6 +1,8 @@
 # This script should index the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
-# The STAR command is provided for you. You should replace the parts surrounded by "<>" and uncomment it.
+# The STAR command is provided for you. You should replace the parts surrounded
+# by "<>" and uncomment it.
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
+# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,5 @@
-# This script should merge all files from a given sample (the sample id is provided in the third argument ($3))
-# into a single file, which should be stored in the output directory specified by the second argument ($2).
+# This script should merge all files from a given sample (the sample id is
+# provided in the third argument ($3)) into a single file, which should be
+# stored in the output directory specified by the second argument ($2).
+#
 # The directory containing the samples is indicated by the first argument ($1).
