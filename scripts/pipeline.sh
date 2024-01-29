42dc495b9d6530efabceec1dfa00c7d908801248
Francisco J. Villena <francis.villena@hotmail.es>
2024-01-29 21:14:41 +0100

    Fixed error in run cutadapt for all merged files

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -19,11 +19,10 @@ do
 done
 
 # TODO: run cutadapt for all merged files
-
 mkdir -p out/trimmed
 mkdir -p log/cutadapt
 
-for sid in $(ls out/merged/*fastq.gz | cut -d "-" -f1 | sed 's:data/::')
+for sid in $(ls out/merged/*fastq.gz | cut -d "-" -f1 | sed 's:out/merged/::')
 do 
 cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
          -o out/trimmed/${sid}.trimmed.fastq.gz out/merged/${sid}.fastq.gz > log/cutadapt/${sid}.log
